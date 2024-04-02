71786fbc4646047d231de6bfd685f0f432e92b53
orson-o <161259576+orson-o@users.noreply.github.com>
2024-04-02 17:18:27 -0500

    Delete nfl_SB_PBP directory for new

--- nfl_SB_PBP/dataExtraction/data_extract.R
+++
@@ -1,8 +0,0 @@
-library(plotly)
-library(tidyverse)
-library(nflfastR)
-sb2000_2023 = fast_scraper_schedules(2000:2023)|>
-  dplyr::filter(game_type == "SB")|>
-  fast_scraper()
-sb_2000_2023 = sb_2000_2023|>
-  mutate(game_seconds_remaining = 3600 - game_seconds_remaining)
